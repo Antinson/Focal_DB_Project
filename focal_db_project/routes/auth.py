c536c537ea4abafa4bc8a0bed10227aa5d6f1409
Ant279a <ant.mitchinson@gmail.com>
2024-06-22 18:11:51 +1200

    Updated auth
    Moved routes from app.py to auth

---
+++ focal_db_project/routes/auth.py
@@ -0,0 +1,35 @@
+from flask import Blueprint, render_template, request, jsonify, redirect, url_for
+from flask_login import login_user, login_required, logout_user, current_user
+from ..models import User
+
+auth_bp = Blueprint('auth', __name__)
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+@auth_bp.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        data = request.json
+        username = data.get('username')
+        password = data.get('password')
+        user = User.query.filter_by(username=username).first()
+        if user and user.password == password:
+            login_user(user)
+            return jsonify({"message": "Login successful"})
+        else:
+            return jsonify({"message": "Invalid credentials"})
+    return render_template('login.html')
+
+@auth_bp.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('login'))
+ 
+
+@login_manager.unauthorized_handler
+def unauthorized():
+    # Redirect unauthorized users to the login page
+    return redirect(url_for('login'))
