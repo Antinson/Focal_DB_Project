570665db93981b05b52462c3135bdad94ac20b60
Ant279a <ant.mitchinson@gmail.com>
2024-06-22 18:00:21 +1200

    Update __init__.py

--- focal_db_project/__init__.py
+++ focal_db_project/__init__.py
@@ -1,15 +1,24 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from flask_login import LoginManager
+
+db = SQLAlchemy()
+migrate = Migrate()
+login_manager = LoginManager()
 
 def create_app():
     app = Flask(__name__)
 
     app.config.from_object('config.Config')
 
-    db = SQLAlchemy(app)
-    migrate = Migrate(app, db)
-    login_manager = LoginManager(app)
+    db.init_app(app)
+    migrate.init_app(app, db)
+    login_manager.login_view = 'auth.login'
 
-    from .routes import main as main_blueprint
-    app.register_blueprint(main_blueprint)
-
+    with app.app_context():
+        from .routes import main, auth
+        app.register_blueprint(main.main_bp)
+        app.register_blueprint(auth.auth_bp)
+    
     return app
