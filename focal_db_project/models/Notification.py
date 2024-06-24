0a87012d62299a496e39fa21635219f536de41e0
Ant279a <ant.mitchinson@gmail.com>
2024-06-24 16:17:10 +1200

    Deleted app.py
    Deleted app.py, seperated into auth.py and main.py using blueprints through wsgi.py

--- focal_db_project/models/Notification.py
+++
@@ -1,9 +0,0 @@
-from datetime import datetime, timedelta
-
-class Notifications():
-
-    def __init__(self, notification_id: int, message: str, user_id: int, timestamp: datetime):
-        self.id = notification_id
-        self.message = message
-        self.user_id = user_id
-        self.timestamp = timestamp
